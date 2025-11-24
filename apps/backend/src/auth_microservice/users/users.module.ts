e67153828301ed00b482cec935d7a635c644e8dd
Turbobot <turbobot@vercel.com>
2025-11-24 17:35:32 +0100

    feat(create-turbo): apply official-starter transform

--- apps/backend/src/auth_microservice/users/users.module.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable, Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-
-export class UsersModule {}
--- apps/backend/src/db_connection.spec.ts
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-describe('Connection do mongo', ()=>{
-  it('should connect', async()=>{
-    const mongouri = 'mongodb://admin:password@localhost:27017/'
-    await mongoose.connect(mongouri)
-    expect(mongoose.connection.readyState).toBe(1)
-    await mongoose.disconnect
-  })
-})
